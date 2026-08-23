e40a620ae4bcaa8d374a4e1d795cbf4d471e2f5d
listrt <rangtt@qq.com>
2026-08-23 22:04:52 +0800

    rth_io_pro

---
+++ output/rth_io_pro.cpp
@@ -0,0 +1,332 @@
+#ifndef rth_io_pro
+#define rth_io_pro
+
+#include<rth_io>
+namespace rttype{
+    static constexpr int ptr_w_rt=sizeof(void*)<<1;
+}
+#if __cplusplus >= 199711L
+namespace rttype{
+    constexpr char hexlist[]={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+    enum state_io{
+        in_=0,out_=1,err_=2
+    };
+    struct oitype{
+        state_io s=err_;
+        int c=0;
+    };
+    static constexpr rttype::oitype out={out_,1};
+    static constexpr rttype::oitype in={in_,1};
+}
+namespace rth_io{
+    template<typename T, typename U>
+    struct is_same {
+       static constexpr int v=0;
+    };
+    template<typename T>
+    struct is_same<T, T> {
+       static constexpr int v=1;
+    };
+    
+    template<typename T>
+    struct is_integer {
+        static const int v=0;
+    };
+    template<typename T>
+    struct is_integer<const T> : is_integer<T> {};
+    template<typename T>
+    struct is_integer<volatile T> : is_integer<T> {};
+    template<typename T>
+    struct is_integer<const volatile T> : is_integer<T> {};
+    #define DEFINE_INTEGER_TYPE(T) \
+    template<> struct is_integer<T> { static const int v=1;};
+    DEFINE_INTEGER_TYPE(bool)
+    DEFINE_INTEGER_TYPE(char)
+    DEFINE_INTEGER_TYPE(signed char)
+    DEFINE_INTEGER_TYPE(unsigned char)
+    DEFINE_INTEGER_TYPE(short)
+    DEFINE_INTEGER_TYPE(unsigned short)
+    DEFINE_INTEGER_TYPE(int)
+    DEFINE_INTEGER_TYPE(unsigned int)
+    DEFINE_INTEGER_TYPE(long)
+    DEFINE_INTEGER_TYPE(unsigned long)
+    DEFINE_INTEGER_TYPE(long long)
+    DEFINE_INTEGER_TYPE(unsigned long long)
+    #undef DEFINE_INTEGER_TYPE
+    //UN
+    template<typename T>
+	struct is_unsigned_integer {
+	    static const int v=0;
+	};
+	
+	template<typename T>
+	struct is_unsigned_integer<const T> : is_unsigned_integer<T> {};
+	template<typename T>
+	struct is_unsigned_integer<volatile T> : is_unsigned_integer<T> {};
+	template<typename T>
+	struct is_unsigned_integer<const volatile T> : is_unsigned_integer<T> {};
+	
+	#define DEFINE_UNSIGNED_INTEGER_TYPE(T) \
+	template<> struct is_unsigned_integer<T> { static const int v=1;};
+	
+	DEFINE_UNSIGNED_INTEGER_TYPE(unsigned short)
+	DEFINE_UNSIGNED_INTEGER_TYPE(unsigned int)
+	DEFINE_UNSIGNED_INTEGER_TYPE(unsigned long)
+	DEFINE_UNSIGNED_INTEGER_TYPE(unsigned long long)
+	
+	#undef DEFINE_UNSIGNED_INTEGER_TYPE
+    template<typename T>
+	struct is_charptr {
+	    static const int v=0;
+	};
+	
+	template<typename T>
+	struct is_charptr<T*> {
+	    static const int v=is_charptr<T>::v;
+	};
+	template<>
+	struct is_charptr<char> {
+	    static const int v=1;
+	};
+	template<>
+	struct is_charptr<const char> {
+	    static const int v=1;
+	};
+	template<>
+	struct is_charptr<volatile char> {
+	    static const int v=1;
+	};
+	template<>
+	struct is_charptr<const volatile char> {
+	    static const int v=1;
+	};
+    //ptr
+    template<typename T>
+    struct is_ptr{static constexpr int v=0;};
+    template<typename T>
+    struct is_ptr<T*>{static constexpr int v=1;};
+
+    static char* lltoa(long long val, char* buf){
+	    char* p=buf+20;
+	    *p^=(*p);
+	    unsigned long long u;
+	    if(val<0){
+	        u = (unsigned long long)(-val);
+	        do{
+	            unsigned long long rem = u % 10;
+	            *--p='0' | (char)rem;
+	            u /= 10;
+	        }while(u);
+	        *--p='-';
+	    }
+	    else{
+	        u=(unsigned long long)val;
+	        do{
+	            unsigned long long rem = u%10;
+	            *--p='0' | (char)rem;
+	            u/=10;
+	        }while (u);
+	    }
+	    return p;
+	}
+	static char* ulltoa(unsigned long long val, char* buf){
+	    char* p=buf+20;
+	    *p^=(*p);
+	    do{
+	        unsigned long long rem = val % 10;
+	        *--p='0' | (char)rem;
+	        val/=10;
+	    }while(val);
+	    return p;
+	}
+    namespace out_fn{
+        static int si(long long a){
+            char c[21];
+            char* p=lltoa(a,c);
+            unsigned len=(c+20)-p;
+            writer(p, len);
+            return 0;
+        }
+        static int ui(unsigned long long a){
+            char c[21];
+            char* p=ulltoa(a,c);
+            unsigned len=(c+20)-p;
+            writer(p, len);
+            return 0;
+        }
+        static int ch(char c){
+            writer(&c,1);
+            return 0;
+        }
+        static int cp(const char* c){
+            unsigned l=0;
+            while(*(c+l++));
+            writer(c,l);
+            return 0;
+        }
+        template<typename T>//ptr
+        static int vp(T* a){
+	        #if __cplusplus>=201103L
+            char c[rttype::ptr_w_rt+2]={[0]='0',[1]='x'};
+            unsigned long long ax=(unsigned long long)a;
+            for (int i=0; i < rttype::ptr_w_rt; i++) {
+			        int shift=(rttype::ptr_w_rt-1-i) <<2;
+			        c[2+i]=rttype::hexlist[(ax>>shift)&0xF];
+			    }
+			writer(c,rttype::ptr_w_rt+2);
+            return 0;
+            #else
+            char c[18];
+            c[0]='0';
+            c[1]='x';
+            unsigned long long ax=(unsigned long long)a;
+            for (int i=0;i<rttype::ptr_w_rt; i++) {
+			        int shift=(rttype::ptr_w_rt-1-i)<<2;
+			        c[2+i]=rttype::hexlist[(ax>>shift)&0xF];
+			}
+            writer(c,18);
+            return 0;
+            #endif
+        }
+        static int fp(long double a) {
+            if (a==0) {
+                writer("0", 1);
+                return 0;
+            }
+            char c[64];
+            int idx=0;
+            if (a < 0) {
+                c[idx++]='-';
+                a=-a;
+            }
+            
+            unsigned long long ip =(unsigned long long)a;
+            long double frac=a-(long double)ip;
+            
+            char buf[32];
+            int bi = 0;
+            do {
+                buf[bi++] = '0' + (int)(ip % 10);
+                ip /= 10;
+            } while (ip);
+            while (bi--) c[idx++] = buf[bi];
+            
+            c[idx++] = '.';
+            
+            for (int i=0;i<6;i++) {
+                frac *= 10;
+                int digit = (int)frac;
+                c[idx++] = '0' + digit;
+                frac -= digit;
+            }
+            
+            while (idx > 0 && c[idx-1] == '0') idx--;
+            if (idx > 0 && c[idx-1] == '.') idx--;
+            
+            writer(c, idx);
+            return 0;
+        }
+    }
+
+    static rttype::oitype outf(float a) {
+        out_fn::fp((long double)a);
+        return {rttype::out_, 1};
+    }
+
+    static rttype::oitype outf(double a) {
+        out_fn::fp((long double)a);
+        return {rttype::out_, 1};
+    }
+
+    static rttype::oitype outf(long double a) {
+        out_fn::fp(a);
+        return {rttype::out_, 1};
+    }
+    static rttype::oitype outf(bool a) {
+        out_fn::fp((long double)a);
+        return {rttype::out_, 1};
+    }
+    template<typename T>
+    static rttype::oitype outf(T* a){
+        out_fn::vp(a);
+        return {rttype::out_, 1};
+    }
+    template<typename T>
+    static rttype::oitype outf(T a) {
+        rttype::oitype result = {rttype::out_, 1};
+        if(rth_io::is_integer<T>::v){
+            if(is_unsigned_integer<T>::v){
+                out_fn::ui((unsigned long long)a);
+                return result;
+            }else{
+                out_fn::si((long long )a);
+                return result;
+            }
+        }else if(rth_io::is_charptr<T>::v){
+            out_fn::cp((const char*)a);
+            return result;
+        }else{
+            out_fn::si(int(a));
+            return result;
+        }
+    }
+}
+
+
+template<typename T>
+static rttype::oitype operator<<(rttype::oitype a, T b) {
+    if (a.c != 1) {
+        rttype::oitype err = {rttype::err_, 0};
+        return err;
+    }
+    
+    if (a.s == rttype::out_) {
+        rth_io::outf(b);
+    } else if (a.s == rttype::in_) {
+        rttype::oitype err = {rttype::err_, 0};
+        return err;
+    }
+    
+    return a;
+}
+
+static inline rttype::oitype operator<<(rttype::oitype a, char* b) {
+    if (a.c != 1 || a.s != rttype::out_) {
+        rttype::oitype err = {rttype::err_, 0};
+        return err;
+    }
+    rth_io::out_fn::cp(b);
+    return a;
+}
+
+static inline rttype::oitype operator<<(rttype::oitype a, const char* b) {
+    if (a.c != 1 || a.s != rttype::out_) {
+        rttype::oitype err = {rttype::err_, 0};
+        return err;
+    }
+    unsigned l = 0;
+    while (b[l]) l++;
+    writer(b, l);
+    return a;
+}
+
+static inline rttype::oitype operator<<(rttype::oitype a, rttype::oitype (*manip)(rttype::oitype)) {
+    return manip(a);
+}
+namespace rttype{
+    static inline rttype::oitype endl(rttype::oitype a) {
+        if (a.s == rttype::out_) {
+            const char n = '\n';
+            writer(&n, 1);
+        }
+        return a;
+    }
+
+    static inline rttype::oitype flush(rttype::oitype a) {
+        return a;
+    }
+}
+
+
+#endif
+#endif/**/
